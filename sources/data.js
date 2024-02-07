ba87eb5c03ae1344ae077f3134390e9340d78913
Maximov1233 <max10050100@gmail.com>
2024-02-07 13:12:26 +0600

    new interface before popup removing

--- sources/data.js
+++ sources/data.js
@@ -1,30 +1,30 @@
 // data of transactions
 
 export let transactionsArr = [
-    {
-        category: 'Shopping',
-        amount: 5000,
-        date: '10/08/2023',
-        comment: ''
-    },
-    {
-        category: 'Maintenance',
-        amount: 1000,
-        date: '11/08/2023',
-        comment: ''
-    },
-    {
-        category: 'Groceries',
-        amount: 4000,
-        date: '12/08/2023',
-        comment: ''
-    },
-    {
-        category: 'Maintenance',
-        amount: 2000,
-        date: '13/08/2023',
-        comment: ''
-    },
+    // {
+    //     category: 'Shopping',
+    //     amount: 5000,
+    //     date: '10/08/2023',
+    //     comment: ''
+    // },
+    // {
+    //     category: 'Maintenance',
+    //     amount: 1000,
+    //     date: '11/08/2023',
+    //     comment: ''
+    // },
+    // {
+    //     category: 'Groceries',
+    //     amount: 4000,
+    //     date: '12/08/2023',
+    //     comment: ''
+    // },
+    // {
+    //     category: 'Maintenance',
+    //     amount: 2000,
+    //     date: '13/08/2023',
+    //     comment: ''
+    // },
     // {
     //     category: 'Mortgage',
     //     amount: 10000,
@@ -33,6 +33,25 @@ export let transactionsArr = [
     // },
 ]
 
+export const categoriesData = ['Shopping', 'Maintenance', 'Groceries', 'Leisure', 'Mortgage', 'Rent'];
+
+export const getCategoryColor = (category) => {
+      switch (category) {
+        case 'Shopping':
+          return 'green';
+        case 'Maintenance':
+          return 'red';
+        case 'Groceries':
+          return 'blue';
+        case 'Leisure':
+          return 'yellow';  
+        case 'Mortgage':
+          return 'violet';
+        case 'Rent':
+          return 'black';  
+      }
+    }
+    
 export const checkAvailability = (arr, val) => {
     return arr.some((arrVal) => val === arrVal);
 }
--- sources/main.js
+++ sources/main.js
@@ -1,72 +1,68 @@
-import { transactionsArr as dataArr, findCategories } from "./data.js";
+import { transactionsArr as dataArr, categoriesData, getCategoryColor, checkAvailability } from "./data.js";
 
-if (!localStorage.transactions) localStorage.setItem('transactions', JSON.stringify(dataArr));
-
-let transactionsArr = JSON.parse(localStorage.getItem('transactions'));
-
-// console.log(JSON.parse(hui));
+// if (!localStorage.transactions) localStorage.setItem('transactions', JSON.stringify(dataArr));
 
 const init = () => {
-  
-  let categoriesArr = findCategories();
 
-  const categorizeFunc = () => { // creates subarrays into other array with same types of categories
-    let outerArr = [];
+  let transactionsArr = JSON.parse(localStorage.getItem('transactions')) || [];
 
-    for (let i = 0; i < categoriesArr.length; i++) {
-      let subArr = [];
-      for (let j = 0; j < transactionsArr.length; j++) {
-        let transactionMatchesCategory = transactionsArr[j].category === categoriesArr[i];
-        if (transactionMatchesCategory) {
-          subArr.push(transactionsArr[j]);
-        }
-      }
-      outerArr.push(subArr);
-    }
+  const addCategoryButton = document.querySelector('.budget-menu__add button');
 
-    return outerArr;
-  }
+  const categoriesSelectionPopup = document.querySelector('.categories-selection-popup'),
+    categoriesSelectionList = categoriesSelectionPopup.querySelector('.categories-selection__list'),
+    categoriesSelectionButton = categoriesSelectionPopup.querySelector('.categories-selection__button button');
 
-  let categorizedArr = categorizeFunc();
-
-  const nameOfMonthUS = new Intl.DateTimeFormat('en-US', { // writes the current date
-    dateStyle: 'medium'
-  }).format(new Date());
-
-  const budgetDate = document.querySelector('.budget-pie__date p');
-  budgetDate.innerHTML = nameOfMonthUS;
-
-  const budgetPieCategories = document.querySelector('.budget-pie__categories');
-
-  const addPopup = document.querySelector('.add-popup'),
-    addCategorySelect = addPopup.querySelector('#categorySelect');
+  let categoriesArr = JSON.parse(localStorage.getItem('categories')) || [];
 
   const categoriesSum = (arr) => { // new summarize of categories (dynamic)
-    let outerArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      let someObj = {};
-      someObj.amount = 0;
-      for (let j = 0; j < arr[i].length; j++) {
-        someObj.category = arr[i][j].category;
-        someObj.amount += arr[i][j].amount;
+    if (arr) { // in case there is some transactions
+      let outerArr = [];
+      for (let i = 0; i < arr.length; i++) { // taking each category
+        let someObj = {};
+        someObj.amount = 0;
+        for (let j = 0; j < arr[i].length; j++) { // then summarizing each transaction inside of it
+          someObj.category = arr[i][j].category;
+          someObj.amount += arr[i][j].amount;
+        }
+        // console.log(someObj);
+        outerArr.push(someObj);
       }
-      outerArr.push(someObj);
+      localStorage.setItem('sums', JSON.stringify(outerArr.flat()));
+      console.log(outerArr.flat());
+      return outerArr.flat();
+    } else { // in case there is no transactions
+      let outerArr = [];
+      for (let i = 0; i < categoriesArr.length; i++) {
+        let someObj = {};
+        someObj.category = categoriesArr[i];
+        someObj.amount = 0;
+        outerArr.push(someObj);
+      }
+      return outerArr;
     }
-    return outerArr.flat();
   };
 
-  const getCategoryColor = (category) => {
-    switch (category) {
-      case 'Shopping':
-        return 'green';
-      case 'Maintenance':
-        return 'red';
-      case 'Groceries':
-        return 'blue';
-      case 'Mortgage':
-        return 'violet';
+  const categoriesCategorize = () => { // creates subarrays into other array with same types of categories
+    let outerArr = [];
+
+    if (transactionsArr.length !== 0) {
+      for (let i = 0; i < categoriesArr.length; i++) {
+        let subArr = [];
+        for (let j = 0; j < transactionsArr.length; j++) {
+          let transactionMatchesWithCategory = transactionsArr[j].category === categoriesArr[i];
+          if (transactionMatchesWithCategory) {
+            subArr.push(transactionsArr[j]);
+          }
+        }
+        outerArr.push(subArr);
+      }
+      return outerArr;
+    } else {
+      return '';
     }
   }
+
+  let categorizedArr = categoriesCategorize();
 
   const biggestAmount = (arr) => {
     let amount = 0;
@@ -87,7 +83,7 @@ const init = () => {
     });
 
     finalArr.forEach((data) => {
-      data.percentage = Math.round(data.amount / sum * 100);
+      data.percentage = Math.round(data.amount / sum * 100) || 0;
       data.color = getCategoryColor(data.category);
     });
 
@@ -97,7 +93,7 @@ const init = () => {
       checkPercentage += data.percentage;
     });
 
-    if (checkPercentage !== 100) {
+    if (checkPercentage !== 100 && checkPercentage > 0) {
       let remainingPercentage = 100 - checkPercentage;
       finalArr.forEach((data) => {
         let theBiggestAmountOfCategories = data.amount === biggestAmount(finalArr);
@@ -110,82 +106,209 @@ const init = () => {
     return finalArr;
   }
 
-  const categoriesList = document.querySelector('.budget-list ul');
+  const categoriesList = document.querySelector('.budget-menu__list ul');
 
-  categoriesArr.forEach((category) => { // creates the list of categories to left of chart
-    let arr = getPercentages(),
-      percentage = 0;
-    arr.forEach((item) => {
-      if (item.category === category) percentage = item.percentage;
+  const categoriesListFill = (categoriesArrNew) => { // creates the list of categories to left of chart
+    categoriesList.innerHTML = '';
+    categoriesArrNew.forEach((category) => {
+      let arr = getPercentages(),
+        percentage = 0;
+      arr.forEach((item) => {
+        if (item.category === category) percentage = item.percentage;
+      });
+      let li = document.createElement('li');
+      li.id = category;
+      let style = li.style;
+      style.setProperty('--listColor', getCategoryColor(category));
+      li.innerHTML = `${category} - ${percentage}%`;
+      categoriesList.appendChild(li);
     });
-    let li = document.createElement('li');
-    li.id = category;
-    let style = li.style;
-    style.setProperty('--listColor', getCategoryColor(category));
-    li.innerHTML = `${category} - ${percentage}%`;
-    categoriesList.appendChild(li);
-  });
+  }
 
-  const categoriesDOM = categoriesList.querySelectorAll('li');
+  categoriesListFill(categoriesArr);
 
-  const categoryPopup = document.querySelector('.category-popup');
+  addCategoryButton.onclick = () => {
+    if (categoriesSelectionPopup.classList.contains('closed')) {
+      categoriesSelectionPopup.classList.toggle('closed');
+      categoriesSelectionList.innerHTML = '';
+      categoriesList.innerHTML = '';
+      categoriesData.forEach((category) => {
+        let categoryDiv = document.createElement('div');
+        categoryDiv.className = 'category';
+        categoryDiv.innerHTML = `
+          <p>${category}</p>
+          <input type="checkbox" id="${category}">
+          `;
+        categoriesSelectionList.appendChild(categoryDiv);
+      });
+
+      let selections = categoriesSelectionList.querySelectorAll('.category');
+
+      selections.forEach((selection) => {
+        if (checkAvailability(categoriesArr, selection.querySelector('p').innerHTML)) {
+          selection.querySelector('input').setAttribute('checked', '');
+        }
+      });
+
+      categoriesSelectionButton.addEventListener('click', () => {
+        const inputs = categoriesSelectionList.querySelectorAll('.category input');
+
+        let categoriesArray = [];
+        inputs.forEach((input) => {
+          if (input.checked) categoriesArray.push(input.id);
+        });
+
+        localStorage.setItem('categories', JSON.stringify(categoriesArray));
+        categoriesListFill(categoriesArray);
+        popupBuild(categoriesArray);
+        categoriesSelectionPopup.classList.add('closed');
+        init();
+      });
+    }
+  }
+
+  const nameOfMonthUS = new Intl.DateTimeFormat('en-US', { // writes the current date
+    dateStyle: 'medium'
+  }).format(new Date());
+
+  const budgetDate = document.querySelector('.budget-pie__date p'); // inserting current date into the circle
+  budgetDate.innerHTML = nameOfMonthUS;
+
+  const sortByAmount = (arr) => {
+    // console.log(arr);
+    let lowestAmount,
+      arrCopy = arr,
+      arrSend = [];
+
+    // console.log(arrCopy);
+
+    const sortingFunc = () => {
+      for (let i = 0; i < arrCopy.length; i++) {
+        if (arrCopy[i].amount < lowestAmount || lowestAmount === undefined) {
+          lowestAmount = arrCopy[i].amount;
+          break;
+        }
+      }
+
+      for (let j = 0; j < arrCopy.length; j++) {
+        if (arrCopy[j].amount === lowestAmount) {
+          arrSend.push(arrCopy[j]);
+          arrCopy.splice(j, 1);
+          break;
+        }
+      }
+    }
+
+    for (let z = 0; z < arr.length; z++) {
+      console.log(arrCopy.length);
+      if (arrCopy.length > 0) sortingFunc();
+    }
+    // console.log(arrCopy);
+    console.log(arrSend);
+    return arrSend;
+  }
+
+  const categoriesDOM = categoriesList.querySelectorAll('li'),
+    categoryPopup = document.querySelector('.category-popup'),
+    categoryPopupList = categoryPopup.querySelector('.category-popup__list'),
+    budgetTransactions = document.querySelector('.budget-transactions'),
+    budgetTransactionsList = budgetTransactions.querySelector('.budget-transactions__list'),
+    budgetTransactionsSum = budgetTransactions.querySelector('.budget-transaction__sum p');
 
   const categoriesDOMBuild = () => {
     for (let i = 0; i < categoriesDOM.length; i++) { // tracks the click and sets the font-size and opacity of pie elements
       categoriesDOM[i].addEventListener('click', () => {
+        
+        let categoriesSumList = JSON.parse(localStorage.getItem('sums'));
+        
+        
+
         const categoriesPie = document.querySelectorAll('.category');
 
         const refresh = () => {
+          budgetTransactionsList.innerHTML = '';
+          budgetTransactionsSum.innerHTML = '';
           categoriesPie.forEach((item) => item.style.opacity = '1');
+          categoryPopup.classList.add('closed');
+          // categoryPopupList.innerHTML = '';
           categoriesDOM.forEach((category) => {
             category.classList.remove('clicked');
             category.style.fontSize = '18px';
-            categoryPopup.classList.add('closed');
-            categoryPopup.innerHTML = ''; 
           });
         }
 
         let categoryIsClicked = categoriesDOM[i].classList.contains('clicked');
+        refresh();
 
-        if (categoryIsClicked) {
-          refresh();
-        } else {
-          refresh();
-          
+        if (!categoryIsClicked) {
           categoriesDOM[i].classList.add('clicked');
           categoriesDOM[i].style.fontSize = '20px';
 
-          // console.log(categorizedArr[i]);
           categoriesPie.forEach((item) => {
-            let itemNotClicked = !item.classList.contains(categoriesDOM[i].id);
-            if (itemNotClicked) {
+            let itemThatIsNotClicked = !item.classList.contains(categoriesDOM[i].id);
+            if (itemThatIsNotClicked) {
               item.style.opacity = '0.3';
             } else {
-              let ourItem = categorizeFunc()[i];
-              console.log(ourItem);
+              categoriesSumList.forEach((sum) => {
 
+                if (item.classList.contains(sum.category)) {
+                  budgetTransactionsSum.innerHTML = `${sum.amount}`;
+                }
+              });
+
+              let ourItem = categoriesCategorize()[i];
+              
               ourItem.forEach((item) => {
                 let transaction = document.createElement('div');
-                transaction.className = 'transaction';
+                transaction.className = `transaction`;
                 transaction.innerHTML = `
                   <div class="transaction-category">Category: ${item.category}</div>
                   <div class="transaction-amount">Amount: ${item.amount}</div>
                   <div class="transaction-date">Date: ${item.date}</div>
                   <div class="transaction-comment">Comment: ${item.comment}</div>
+                  <div class="transaction-delete">
+                    <button type="submit">Delete</button>
+                  </div>
                 `;
-                categoryPopup.classList.remove('closed');
-                categoryPopup.appendChild(transaction);
+                budgetTransactionsList.appendChild(transaction);
               });
-              
-              // categorizeFunc().forEach((item) => {
-              //   console.log(item);
-              //   item.forEach((subItem) => {
-              //     if (subItem.category === categoriesDOM[i].id) return item;
-              //   });
-                // if (categoriesDOM[i].id === item.category) console.log(item);
-              // });
+              // budgetTransactions.appendChild(budgetTransactionsList);
+              // categoryPopup.classList.remove('closed');
             }
           });
+
+          const categoryPopupSort = categoryPopup.querySelector('.category-popup__sort'),
+            categoryPopupSortButton = categoryPopupSort.querySelector('button');
+
+          categoryPopupSortButton.addEventListener('click', () => {
+            const sortKind = categoryPopupSort.querySelector('select').value;
+            if (sortKind === 'amount') {
+              let bizdinItem = categoriesCategorize()[i];
+              console.log(bizdinItem);
+              let sendingItem = bizdinItem;
+              console.log(sendingItem);
+              let sortedArr = sortByAmount(sendingItem);
+              console.log(sortedArr);
+            }
+          });
+
+          const transactionsPopup = document.querySelectorAll('.transaction');
+          for (let j = 0; j < transactionsPopup.length; j++) {
+            let deleteButton = transactionsPopup[j].querySelector('button');
+
+            deleteButton.addEventListener('click', () => {
+              let transactionsArrDel = categoriesCategorize();
+              transactionsArrDel[i].splice(j, 1);
+
+              console.log(transactionsArrDel.flat());
+
+              localStorage.removeItem('transactions');
+              localStorage.setItem('transactions', JSON.stringify(transactionsArrDel.flat()));
+              categoryPopup.classList.add('closed');
+              alert('Transaction has been deleted');
+              init();
+            });
+          }
         }
       });
     }
@@ -193,12 +316,12 @@ const init = () => {
 
   categoriesDOMBuild();
 
-  let categories = getPercentages();
+  let categoriesWithPercentages = getPercentages();
 
   const getRotationData = () => { // gathers information about rotating the segments as categories
     let sum = [];
 
-    categories.forEach((category) => {
+    categoriesWithPercentages.forEach((category) => {
       sum.push(parseInt(category.percentage));
     });
 
@@ -215,7 +338,10 @@ const init = () => {
     return newArr;
   }
 
+  const budgetPieCategories = document.querySelector('.budget-pie__categories');
+
   const categoryFill = (categories) => { // creates div element of category and puts it into html
+    budgetPieCategories.innerHTML = '';
     for (let i = 0; i < categories.length; i++) {
       let category = document.createElement('div');
       category.className = `category ${categories[i].category}`;
@@ -232,55 +358,61 @@ const init = () => {
     }
   }
 
+  const addTransactionPopup = document.querySelector('.add-transaction-popup'),
+    addTransactionCategorySelect = addTransactionPopup.querySelector('#categorySelect');
+
   const popupBuild = (categories) => { // creates the popup to add transaction
-    categories.forEach((category) => {
-      let option = document.createElement('option');
-      option.value = `${category.category}`;
-      option.innerHTML = `${category.category}`;
-      addCategorySelect.appendChild(option);
-    });
+    addTransactionCategorySelect.innerHTML = '';
+    if (categoriesArr) {
+      categories.forEach((category) => {
+        let option = document.createElement('option');
+        option.value = `${category}`;
+        option.innerHTML = `${category}`;
+        addTransactionCategorySelect.appendChild(option);
+      });
+    }
   }
 
-  categoryFill(categories);
-  popupBuild(categories);
+  popupBuild(categoriesArr);
+  categoryFill(categoriesWithPercentages);
 
   const addDataButton = document.querySelector('.budget-add button');
 
-  addDataButton.addEventListener('click', () => {
-    addPopup.classList.toggle('closed');
-  });
+  addDataButton.onclick = () => {
+    if (categoriesArr[1]) {
+      addTransactionPopup.classList.toggle('closed');
+    } else {
+      alert('Choose your categories first!');
+    }
+  }
 
   const dataSubmitButton = document.querySelector('.category-submit button');
 
-  dataSubmitButton.addEventListener('click', () => {
-    let categoryInput = addCategorySelect.value,
-      amountInput = parseInt(addPopup.querySelector('.category-value input').value),
-      commentInput = addPopup.querySelector('.category-info input').value;
+  dataSubmitButton.onclick = () => {
+    let categoryInput = addTransactionCategorySelect.value,
+      amountInput = parseInt(addTransactionPopup.querySelector('.category-value input').value),
+      commentInput = addTransactionPopup.querySelector('.category-info input').value,
+      dateInput = addTransactionPopup.querySelector('.category-date input').value;
 
-    if (commentInput === '') commentInput = 'No comment';
-
-    let newTime = new Intl.DateTimeFormat('en-UK', { // writes the current date
-      dateStyle: 'short'
-    }).format(new Date());
-
-    if (amountInput) {
+    if (amountInput > 0 && dateInput) {
       let newData = {
         category: categoryInput,
         amount: amountInput,
-        date: newTime,
-        comment: commentInput
+        date: dateInput,
+        comment: commentInput ? commentInput : 'No comment'
       };
       transactionsArr.push(newData);
+      console.log(newData);
       localStorage.removeItem('transactions');
       localStorage.setItem('transactions', JSON.stringify(transactionsArr));
       categoriesList.innerHTML = '';
-      addPopup.classList.toggle('closed');
+      addTransactionPopup.classList.toggle('closed');
       alert('Transaction has been added');
       init();
     } else {
-      alert('Fill the amount');
+      alert('Fill the amount and date');
     }
-  });
+  }
 }
 
 init();
